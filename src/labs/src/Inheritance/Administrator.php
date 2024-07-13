fd5552501d7e15fa111e32d489c77ea766628338
Brenden Koenigsman <brenden.koenigsman@greatplainsmfg.com>
2024-07-12 22:21:25 -0500

    renamed and added a method on Admin class

--- src/labs/src/Inheritance/Administrator.php
+++ src/labs/src/Inheritance/Administrator.php
@@ -6,12 +6,17 @@ use ZendOopCourse\Labs\Inheritance\SuperUser;
 
 class Administrator extends SuperUser
 {
+    public function setUsername($newUsername)
+    {
+        $this->username = $newUsername;
+    }
+
     public function setUsernameForUser(&$user, string $newUsername)
     {
         $user->username = $newUsername;
     }
 
-    public function changePasswordForUser(&$user, string $newPassword)
+    public function setPasswordForUser(&$user, string $newPassword)
     {
         $user->password = $newPassword;
     }
