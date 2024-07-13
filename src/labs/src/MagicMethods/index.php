73fd12e084080f3ddf23fb8ef82be74b97747bb7
Brenden Koenigsman <brenden.koenigsman@greatplainsmfg.com>
2024-07-13 00:02:14 -0500

    Magic Methods Lab

--- src/labs/src/MagicMethods/index.php
+++ src/labs/src/MagicMethods/index.php
@@ -6,8 +6,10 @@ use ZendOopCourse\Labs\MagicMethods\SuperUser;
 // __construct();
 $me = new SuperUser('brendenk', 'abc123', 'Brenden', 'Koenigsman');
 // __toString()
-echo 'Me: ' . $me;
+echo 'New SuperUser: ' . $me;
 echo PHP_EOL;
 
 // __call(): trying to access admin method from a parent class
 $me->setUsername('b_koenigsman');
+
+// __destruct(): used for logging new or modified users in user_activity.log
